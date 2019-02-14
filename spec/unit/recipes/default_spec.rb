e51e05ad7cfbbb52259aab6e7e40260c6c0a468e
Laura Guyon <laura.daras@gmail.com>
2019-02-14 14:56:00 +0100

    add some default recipe and unit test

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,35 +1,31 @@
-#
-# Cookbook:: devessentials
-# Spec:: default
-#
-# Copyright:: 2019, The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
 describe 'devessentials::default' do
   context 'When all attributes are default, on Ubuntu 16.04' do
+    let(:conf) do
+      { platform: 'ubuntu', version: '14.04' }
+    end
+
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(conf)
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it 'installs package httpd' do
+      expect(chef_run).to install_package 'httpd'
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'enables service httpd' do
+        expect(chef_run).to enable_service 'httpd'
+    end
+
+    it 'starts service httpd' do
+      expect(chef_run).to start_service 'httpd'
     end
   end
 end
